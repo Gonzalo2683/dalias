68ba7d3ff928dad9fe98693126aafe0b204b5b06
Gonzalo Guevara <gonzarock@yahoo.com>
2018-07-24 18:16:23 -0300

    Primier commit

---
+++ jutep.aliases.drushrc.php
@@ -0,0 +1,9 @@
+<?php
+
+# File: jutep.aliases.drushrc.php  
+
+$aliases['local'] = array(
+  'root' => '/var/www/html/',
+  'uri' => 'portalbases-jutep.ddev.local',
+); 
+
---
+++ uaip.aliases.drushrc.php
@@ -0,0 +1,9 @@
+<?php
+
+# File: uaip.aliases.drushrc.php  
+
+$aliases['local'] = array(
+  'root' => '/var/www/html/',
+  'uri' => 'portalbases-uaip.ddev.local',
+); 
+
